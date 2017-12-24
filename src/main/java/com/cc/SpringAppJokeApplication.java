202876af9f5613b48f79722c08b23133e51e3a6c
Wyyx <gnehcyx@163.com>
2017-12-24 14:56:18 +0800

    Init commit

---
+++ src/main/java/com/cc/SpringAppJokeApplication.java
@@ -0,0 +1,12 @@
+package com.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppJokeApplication.class, args);
+	}
+}
